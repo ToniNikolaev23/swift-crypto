544430273d6736c954f8e6d0227a19c995f70e29
Toni Stoyanov <toni.n.stoyanov@icloud.com>
2024-12-25 14:05:29 +0200

    Initial Commit

---
+++ SwiftCrypto/SwiftCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCryptoApp.swift
+//  SwiftCrypto
+//
+//  Created by Toni Stoyanov on 25.12.24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
